9c12563a86fcc7b984ead5c918175174bc9c2d7e
Thomas Hessler <thomas@meanguppy.com>
2020-05-09 11:45:50 -0400

    Updates

---
+++ precache-manifest.1e0d0721cddeb7b864460520b11dece1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "015f02f1f1d2d4606325579b915e2d23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9d35ec8e9b6ae46014a",
+    "url": "/static/js/2.4b034889.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4b034889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc71bf38e4e90fafb5ab",
+    "url": "/static/js/main.b9fa0dfd.chunk.js"
+  },
+  {
+    "revision": "8b858d3d7f31e57d08b4",
+    "url": "/static/js/runtime-main.b12d22f4.js"
+  }
+]);
